b86cd6241e931685ac5497348ed022bebf802ca8
Kayathri2218 <kaya3kayu2000@gmail.com>
2023-02-11 20:32:30 +0530

    update javascript file.

--- main.js
+++ main.js
@@ -1,30 +1,4 @@
-// let count = 0;
-//   const output = document.querySelector("#output1");
 
-//   const increment = document.querySelector("#increment1");
-//   increment.addEventListener("click", function() {
-//     count++;
-//     output.value = count;
-//   });
-
-//   const decrement = document.querySelector("#decrement1");
-//   decrement.addEventListener("click", function() {
-//     if (count > 0) { 
-//       count--; 
-//     }
-//     output.value = count;
-//   });
-
-
-// let count1 = 0;
-// for(i=0;i<=count1;i++){
-//   if(count1<0){
-//     let inc = document.getElementById("#out");
-//   }
-//   else if (count1>0){
-//     let dec = document.getElementById("#out")
-//   }
-// }
 
 let btn = document.getElementById("increment");
 let btn1 = document.getElementById("decrement");
